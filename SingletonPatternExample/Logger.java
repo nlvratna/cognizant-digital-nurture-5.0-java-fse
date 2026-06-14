c17dabb15fa1cd063d44dd3b2de9583eab396bb9
nlvratna <ratnachowdary56@gmail.com>
2026-06-14 13:52:06 +0530

    logger class

---
+++ SingletonPatternExample/Logger.java
@@ -0,0 +1,17 @@
+class Logger {
+  private static Logger instance;
+
+  private Logger() {}
+
+  public static Logger getInstance() {
+    if (instance == null) {
+      instance = new Logger();
+    }
+
+    return instance;
+  }
+
+  public void log(MessageType type, String message) {
+    System.out.println(type + " " + message);
+  }
+}
