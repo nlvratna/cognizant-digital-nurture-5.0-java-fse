33048e7e4f647b540160addc03997e4412e440b4
nlvratna <ratnachowdary56@gmail.com>
2026-06-23 19:31:41 +0530

    completed spring core exercise 2

--- week 2/module 5/LibrarayManagementApplication/src/main/java/com/library/App.java
+++ week 2/module 5/LibrarayManagementApplication/src/main/java/com/library/App.java
@@ -1,9 +1,16 @@
 package com.library;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
     public static void main(String[] args) {
 
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
+
+        BookService bookService = appContext.getBean(BookService.class);
+        bookService.inject();
     }
 
 }
