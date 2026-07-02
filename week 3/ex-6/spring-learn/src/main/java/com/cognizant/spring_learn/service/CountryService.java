b967750c107fe7550154d89512286671bccf0ded
nlvratna <ratnachowdary56@gmail.com>
2026-07-02 18:11:24 +0530

    return jwt exercise

---
+++ week 3/ex-6/spring-learn/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,33 @@
+package com.cognizant.spring_learn.service;
+
+import com.cognizant.spring_learn.model.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CountryService {
+
+    private ArrayList<Country> countries;
+
+    public CountryService() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        this.countries = context.getBean("countryList", ArrayList.class);
+    }
+
+    public Country getCountry(String code) {
+        for (Country country : countries) {
+            if (code.equalsIgnoreCase(country.getCode())) {
+                return country;
+            }
+        }
+        return null;
+    }
+
+    public List<Country> getCountries() {
+        return this.countries;
+    }
+}
